cfa776f20c46817ac3f026a63162b10f7031a08d
Cole Dahlen <coledahlen@gmail.com>
2023-03-06 10:32:19 -0600

    added database

--- database.sql
+++ database.sql
@@ -1,10 +1,35 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR(500) NOT NULL,
+	"password" VARCHAR(500) NOT NULL
+);
+CREATE TABLE "events" (
+	"id" SERIAL PRIMARY KEY,
+	"event_host_id" INT REFERENCES "user",
+	"event_name" VARCHAR(500) NOT NULL,
+	"deadline" DATE NOT NULL,
+	"location" VARCHAR(500) NOT NULL,
+	"event_password" INTEGER NOT NULL,
+	"event_date" DATE NOT NULL
+);
+CREATE TABLE "guests" (
+	"id" SERIAL PRIMARY KEY,
+	"party_id" INT REFERENCES "party",
+	"name" VARCHAR(500) NOT NULL,
+	"response" BOOLEAN DEFAULT FALSE,
+	"meal_id" INTEGER NOT NULL,
+	"phone_number" INTEGER NOT NULL,
+	"email_address" VARCHAR(500) NOT NULL
+);
+CREATE TABLE "meal_options" (
+	"id" SERIAL PRIMARY KEY,
+	"event_id" INT REFERENCES "events",
+	"meal_name" VARCHAR(500) NOT NULL,
+	"description" VARCHAR(500)
+);
+
+CREATE TABLE "party" (
+	"id" SERIAL PRIMARY KEY,
+	"event_id" INT REFERENCES "events",
+	"name" VARCHAR(500) NOT NULL
 );
