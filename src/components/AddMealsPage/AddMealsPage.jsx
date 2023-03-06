d6f4735bb8f46a0ae87e8ba39df249fe411023ce
Jeremy Hanson <rockbasser88@hotmail.com>
2023-03-06 13:57:50 -0600

    text fields render incrementally

--- src/components/AddMealsPage/AddMealsPage.jsx
+++ src/components/AddMealsPage/AddMealsPage.jsx
@@ -1,18 +1,52 @@
 import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name AddMealsPage with the name for the new component.
-function AddMealsPage(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-  const store = useSelector((store) => store);
-  const [heading, setHeading] = useState('Functional Component');
+function AddMealsPage() {
+  const [textFields, setTextFields] = useState([textFields]);
+
+  const addInput = () => {
+    console.log('add input:');
+
+    setTextFields(textFields.concat(''))
+  }
 
   return (
     <div>
-      <h2>{heading}</h2>
+      <h2>Add Meals</h2>
+      <p>How many meals?</p>
+      <Button 
+        variant="contained"
+        onClick={addInput}
+      >Add Meal+
+      </Button>
+      {/* <TextField 
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Meal 1"
+      /> */}
+      {textFields.map((field, index) => (
+        <div>
+          <TextField 
+            required 
+            id="outlined-required" 
+            key={index} 
+            label={`Meal Option ${index + 1}`}
+            placeholder={`Meal Option ${index + 1}`}
+          />
+          <TextField 
+            required 
+            id="outlined-required" 
+            key={index} 
+            label={`Meal Description ${index + 1}`}
+            placeholder={`Meal Description ${index + 1}`}
+          />
+        </div>
+      ))}
     </div>
   );
 }
