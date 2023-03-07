d120a14204689c7836d92443877370b117eeb895
Laura McGowan <prime.lmcg@gmail.com>
2023-03-07 09:09:32 -0600

    added comments to page

--- src/components/SelectPartyPage/SelectPartyPage.jsx
+++ src/components/SelectPartyPage/SelectPartyPage.jsx
@@ -11,24 +11,26 @@ import Button from '@mui/material/Button';
 import styled from "styled-components";
 import { Grid } from "@mui/material";
 
-// const PartyNameButton = styled.button
-// `
-//   .button-text {
-//     text-transform: none;
+// Alternate way to style...
+// From Laura's solo project
+// const styles = {
+//   padding: {
+//     paddingTop: "20px",
+//     marginTop: "20px"
 //   }
-//   `;
+// }
+// In the return:
+// <div className="card mb-3" sx={{maxWidth: '540px'}} style={styles.padding}>
+
+
 
 function SelectPartyPage() {
 
-
- 
   const params = useParams();
   const dispatch= useDispatch();
   const history = useHistory();
 
   const partyNames = useSelector((store) => store.partyNames);
-
-  
 
   useEffect(() => {
     const eventCode=params.id
@@ -41,47 +43,45 @@ function SelectPartyPage() {
 
   return (
     <>
-      <h4 style={{marginLeft:"15px"}}>Welcome, please select your party:</h4>
+      <h4 style={{marginLeft:"15px", fontFamily:"roboto"}}>Welcome, please select your party:</h4>
 
 
-    {/* Party Names Listed */}
+    {/* Map through Party Names - each name listed on its own button */}
       <div>
-    <Grid container direction="column">
-        {partyNames.map(partyName => (    
-          <Button 
-            key={partyName.id}
-            className="PartyNameButton"
-            type="name"
-            variant="contained" 
-            size="small"
-            style={{backgroundColor:"#4330DA", 
+        <Grid container direction="column">
+          {partyNames.map(partyName => (    
+            <Button 
+              key={partyName.id}
+              className="PartyNameButton"
+              type="name"
+              variant="contained" 
+              size="small"
+              style={{backgroundColor:"#4330DA", 
                     textTransform:"none", 
                     marginTop:"10px",
                     marginLeft:"20px",
                     marginRight:"20px"
                   }}
-            onClick={() => history.push('/rsvp')}>
-            {partyName.name}
-          </Button>
+              onClick={() => history.push('/rsvp')}>
+              {partyName.name}
+            </Button>
            ))}
-    </Grid>
-       
+        </Grid>
       </div>
 
 
 
-    {/* Back Button */}
-    <Button 
-      className="backToEventCodePage"
-      type="back"
-      variant="outlined" 
-      style={{color:"#4330DA", 
+    {/* Back Button - will send user back to the Event Code page */}
+      <Button 
+        className="backToEventCodePage"
+        type="back"
+        variant="outlined" 
+        style={{color:"#4330DA", 
               border:"2px solid #4330DA", 
               marginTop:"25px",
               marginLeft:"20px"}}
-      onClick={() => history.push('/eventCode')}>Back
-    </Button>
-
+        onClick={() => history.push('/eventCode')}>Back
+      </Button>
     </>
   );
 }
