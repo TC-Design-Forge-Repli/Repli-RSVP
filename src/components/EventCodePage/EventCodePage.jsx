7bde394c26776bf590fe377a25446ac43469b7a0
Laura McGowan <prime.lmcg@gmail.com>
2023-03-14 11:34:59 -0500

    was coding on landingPage instead of eventCode page...landingPage back to its original state

--- src/components/EventCodePage/EventCodePage.jsx
+++ src/components/EventCodePage/EventCodePage.jsx
@@ -54,6 +54,46 @@ function EventCodePage() {
 
 
   return (
+
+
+    <header style={{ paddingLeft: 0 }}>
+    <div
+      className='p-5 text-center bg-image'
+      // style={{backgroundImage:"url(./images/repliHero.jpg)"}}
+
+      style={{ backgroundImage: "url('https://media.gettyimages.com/id/165503175/vector/brunette-bride-and-groom.jpg?s=612x612&w=gi&k=20&c=AisK8hgse_fUNiJUAoo_UqqoY2WTWCJGY-QNCCuyyOk=')", 
+               height: 800, 
+               backgroundRepeat: "no-repeat" }}
+    >
+      {/* <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}> */}
+        <div className='d-flex justify-content-center align-items-center h-100'>
+          <div className='text-white'>
+            <h1 className='mb-3'>Welcome to Repli...</h1>
+            <h4 className='mb-3'>RSVP here for the big day.</h4>
+
+            <TextField
+          // required
+          id="outlined"
+          label="Enter your event code"
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              "& > fieldset": { borderColor: "#4330DA", backgroundColor:"#E34973"},
+            },
+            "& .MuiOutlinedInput-root.Mui-focused": {
+              "& > fieldset":{borderColor:"#4330DA"}
+            },
+            "& label.Mui-focused":{color:"#4330DA"},
+            margin:"5px"
+          }}
+          // defaultValue="Email"
+          // value={email}
+          // onChange={(event) => setEmail(event.target.value)}
+        />
+        </div>
+        </div>
+      </div>
+
+
     <form>
       <TextField
         className="eventCodeInput"
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -2,17 +2,6 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './LandingPage.css';
 
-//MUI Imports
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
-
-
-
-
-
-
-
 // CUSTOM COMPONENTS
 import RegisterForm from '../RegisterForm/RegisterForm';
 
@@ -20,104 +9,60 @@ function LandingPage() {
   const [heading, setHeading] = useState('Welcome');
   const history = useHistory();
 
-  
-
-
-
-
-
-
-
-
-
-
   const onLogin = (event) => {
     history.push('/login');
   };
 
   return (
-<>
-    <header style={{ paddingLeft: 0 }}>
-    <div
-      className='p-5 text-center bg-image'
-      // style={{backgroundImage:"url(./images/hotairBalloon.png)"}}
+    <div className="container">
+      <h2>{heading}</h2>
 
-      style={{ backgroundImage: "url('https://media.gettyimages.com/id/165503175/vector/brunette-bride-and-groom.jpg?s=612x612&w=gi&k=20&c=AisK8hgse_fUNiJUAoo_UqqoY2WTWCJGY-QNCCuyyOk=')", 
-               height: 800, 
-               backgroundRepeat: "no-repeat" }}
-    >
-      {/* <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}> */}
-        <div className='d-flex justify-content-center align-items-center h-100'>
-          <div className='text-white'>
-            <h1 className='mb-3'>Welcome to Repli...</h1>
-            <h4 className='mb-3'>RSVP here for the big day.</h4>
+      <div className="grid">
+        <div className="grid-col grid-col_8">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
+            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
+            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
+            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
+            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
+            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
+            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
+            luctus. Duis a sapien metus.
+          </p>
 
-            <TextField
-          // required
-          id="outlined"
-          label="Enter your event code"
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "& > fieldset": { borderColor: "#4330DA", backgroundColor:"#E34973"},
-            },
-            "& .MuiOutlinedInput-root.Mui-focused": {
-              "& > fieldset":{borderColor:"#4330DA"}
-            },
-            "& label.Mui-focused":{color:"#4330DA"},
-            margin:"5px"
-          }}
-          // defaultValue="Email"
-          // value={email}
-          // onChange={(event) => setEmail(event.target.value)}
-        />
+          <p>
+            Praesent consectetur orci dui, id elementum eros facilisis id. Sed
+            id dolor in augue porttitor faucibus eget sit amet ante. Nunc
+            consectetur placerat pharetra. Aenean gravida ex ut erat commodo, ut
+            finibus metus facilisis. Nullam eget lectus non urna rhoncus
+            accumsan quis id massa. Curabitur sit amet dolor nisl. Proin
+            euismod, augue at condimentum rhoncus, massa lorem semper lacus, sed
+            lobortis augue mi vel felis. Duis ultrices sapien at est convallis
+            congue.
+          </p>
 
-
-
-
-
-{/* 
-            <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
-              Enter Your Event Code Here
-            </a> */}
-          </div>
+          <p>
+            Fusce porta diam ac tortor elementum, ut imperdiet metus volutpat.
+            Suspendisse posuere dapibus maximus. Aliquam vitae felis libero. In
+            vehicula sapien at semper ultrices. Vivamus sed feugiat libero. Sed
+            sagittis neque id diam euismod, ut egestas felis ultricies. Nullam
+            non fermentum mauris. Sed in enim ac turpis faucibus pretium in sit
+            amet nisi.
+          </p>
         </div>
-      {/* </div> */}
+        <div className="grid-col grid-col_4">
+          <RegisterForm />
+
+          <center>
+            <h4>Already a Member?</h4>
+            <button className="btn btn_sizeSm" onClick={onLogin}>
+              Login
+            </button>
+          </center>
+        </div>
+      </div>
     </div>
-  </header>
-  </>
-
-  
-  
-    // <div className="container">
-    //   <h2>{heading}</h2>
-    //   <Box
-    //   sx={{
-    //     width: 300,
-    //     height: 300,
-    //     backgroundColor: 'primary.dark',
-    //     '&:hover': {
-    //       backgroundColor: 'primary.main',
-    //       opacity: [0.9, 0.8, 0.7],
-    //     },
-    //   }}
-    // />
-
-    //   <div className="grid">
-    //     <div className="grid-col grid-col_8">
-    //       {/*  */}
-    //     </div>
-    //     <div className="grid-col grid-col_4">
-    //       <RegisterForm />
-
-    //       <center>
-    //         <h4>Already a Member?</h4>
-    //         <button className="btn btn_sizeSm" onClick={onLogin}>
-    //           Login
-    //         </button>
-    //       </center>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
